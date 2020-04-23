5f119d62569b9bbbba1571097ace692922b77341
Cecilia <ceci2hdz@gmail.com>
2020-04-22 20:50:49 -0500

    Create migration Travel, Transport, User.

---
+++ app/Transport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transport extends Model
+{
+    protected $fillable = [
+        'id',
+        'description',
+        'color',
+        'licensePlate',
+        'brand',
+        'model',
+        'typeTransport'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
